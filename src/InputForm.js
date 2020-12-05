2d899fe2f0973f9fad45ae844decd424e6d288f9
Shathath <shathathrahman@gmail.com>
2020-12-05 17:40:18 +0530

    basic form added

---
+++ src/InputForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class InputForm extends React.Component{
+	render(){
+		return(
+			<div>
+				<input type='text'></input>
+				<button type='submit'>Add User</button>
+			</div>
+			
+		)
+	}
+}
